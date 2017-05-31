0af57b74b7357756acd1ec5e41c7afb62b212091
William Chan <root@williamchan.me>
2017-05-31 16:59:33 +0800

    added px2rem-loader

--- build/utils.js
+++ build/utils.js
@@ -16,13 +16,18 @@ exports.cssLoaders = function (options) {
     loader: 'css-loader',
     options: {
       minimize: process.env.NODE_ENV === 'production',
-      sourceMap: options.sourceMap
+      sourceMap: options.sourceMap,
     }
   }
-
+  var px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 75,
+    }
+  }
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
+    var loaders = [cssLoader, px2remLoader]
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
