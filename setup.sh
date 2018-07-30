e541057c7d477e1000c666d258f0767f6c3cb5d7
Emil Zhai <root@derzh.com>
2018-07-30 13:28:20 +0800

    Fix setup.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 if [ ! -f node_modules/package.gitlab-ci.json ] || ! cmp -s package.json node_modules/package.gitlab-ci.json\
   # || [ ! -f node_modules/package-lock.gitlab-ci.json ] || ! cmp -s package-lock.json node_modules/package-lock.gitlab-ci.json
 then
+  mkdir -p node_modules
   cp -rf package.json node_modules/package.gitlab-ci.json.tmp
   # cp -rf package-lock.json node_modules/package-lock.gitlab-ci.json.tmp
   # npm config set registry https://registry.npm.taobao.org
