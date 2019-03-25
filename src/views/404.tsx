bc306111d527de769b6a880e4fc601886a673457
Emil Zhai <root@derzh.com>
2019-03-25 16:24:51 +0800

    feat: use tsx for 404 page

---
+++ src/views/404.tsx
@@ -0,0 +1,31 @@
+import Vue, { VNode } from 'vue';
+import styles from '@/styles/views/404.module.scss';
+
+// 原理是先过一遍 ts-loader 然后在过 babel-loader
+// 比较坑就是了
+// .tsx 后缀直接走 babel-loader
+
+export default Vue.extend({
+  mounted() {
+    console.log('hello 404'); // eslint-disable-line
+  },
+  methods: {
+    test() {
+      const name: string = 'hello';
+      console.log(name); // eslint-disable-line
+    },
+  },
+  render(): VNode {
+    const b = [1, 2, 3];
+    return (
+      <div onClick={this.test} class={styles.a404}>
+        { b.map(() => <br />) }
+        <center>页面不存在</center>
+        <br />
+        <br />
+        <center><router-link tag="button" to={{ name: 'index' }}>返回主页</router-link></center>
+        <center style="color: #ddd">This project started building with typescript</center>
+      </div>
+    );
+  },
+});
