cd12a410bb65d015fbd76914e8d02a9fc3bdaac8
Emil Zhai <root@derzh.com>
2019-04-09 17:58:58 +0800

    feat: optimize popup page initial time

--- src/views/popup/index.vue
+++ src/views/popup/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="menu">
-    <button @click="popupUser" type="primary" class="menu-item">User center</button>
+    <template v-if="mounted">
+      <button @click="popupUser" type="primary" class="menu-item">User center</button>
+    </template>
   </div>
 </template>
 <script>
 import { popupWindow } from '@/utils/chrome-ext';
 
 export default {
+  data() {
+    return {
+      mounted: false,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.startRender);
+    this.timerRender = window.setTimeout(this.startRender, 50);
+  },
   methods: {
     popupUser() {
       popupWindow('index.html#/user', true);
+    },
+    startRender() {
+      if (this.timerRender) {
+        clearTimeout(this.timerRender);
+        delete this.timerRender;
+      }
+      this.mounted = true;
+      window.removeEventListener('resize', this.onMounted);
     },
   },
 };
