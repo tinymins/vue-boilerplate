42f298c638879ed7421460d72ef2b8d6f3ed6a12
Emil Zhai <root@derzh.com>
2020-02-09 19:40:05 +0800

    feat: move static vue init logic code

--- src/global/create-vue.ts
+++ src/global/create-vue.ts
@@ -28,6 +28,88 @@ import { configWechatSDK } from '@/utils/connect';
 import { isInMobileDevice } from '@/utils/environment';
 import ViewportControl from './viewport-control';
 
+// Register the router hooks with their names
+Component.registerHooks([
+  'asyncData',
+  'onWechatReady',
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate', // for vue-router 2.2+
+]);
+
+// Init Vue
+Vue.config.productionTip = false;
+Vue.use(StoreUtils);
+Vue.use(PortalVue);
+Vue.use(PreventOverscroll);
+Vue.use(Wechat);
+Vue.use(PhotoSwipe, {
+  pswpOptions: {
+    showShare: false,
+    showZoom: false,
+    showArrow: false,
+    showFullscreen: false,
+  },
+  wechat,
+});
+Vue.use(VueLazyload);
+
+Object.defineProperty(Vue.prototype, '$routeInfo', { get() { return routeClone(this.$route); } });
+
+Vue.mixin(Vue.extend({
+  beforeMount() {
+    // Deal with Vue.use() of current component.
+    const uses = this.$options.uses;
+    if (uses) {
+      uses.forEach(entity => Vue.use(entity));
+    }
+  },
+  mounted() {
+    const proto = Object.getPrototypeOf(this);
+    const options = this.$options.options || get(proto, 'constructor.options.options');
+    if (options) {
+      if (options.hideTabbar !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.SET_TABBAR_VISIBLE}`, { id: this._uid, value: !options.hideTabbar });
+      }
+      if (options.hideNavbar !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.SET_NAVBAR_VISIBLE}`, { id: this._uid, value: !options.hideNavbar });
+      }
+      if (options.bodyAutoHeight !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.SET_BODY_AUTO_HEIGHT}`, { id: this._uid, value: options.bodyAutoHeight });
+      }
+      if (options.bodyBackground !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.SET_BODY_BACKGROUND}`, { id: this._uid, value: options.bodyBackground });
+      }
+    }
+    const onWechatReady = this.$options.onWechatReady;
+    if (onWechatReady) {
+      configWechatSDK(this.$store).then(() => {
+        this.$wechat.ready(() => {
+          onWechatReady.call(this);
+        });
+      });
+    }
+  },
+  destroyed() {
+    const proto = Object.getPrototypeOf(this);
+    const options = this.$options.options || get(proto, 'constructor.options.options');
+    if (options) {
+      if (options.hideTabbar !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.REMOVE_TABBAR_VISIBLE}`, { id: this._uid });
+      }
+      if (options.hideNavbar !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.REMOVE_NAVBAR_VISIBLE}`, { id: this._uid });
+      }
+      if (options.bodyAutoHeight !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.REMOVE_BODY_AUTO_HEIGHT}`, { id: this._uid });
+      }
+      if (options.bodyBackground !== void 0) {
+        this.$store.commit(`common/bus/${COMMON.REMOVE_BODY_BACKGROUND}`, { id: this._uid });
+      }
+    }
+  },
+}));
+
 const createVue = (store: StoreInstance, router: RouterInstance): Vue => {
   // Mount utils
   if (isInMobileDevice(store.state.common.app.entryParams.userAgent)) {
@@ -35,88 +117,6 @@ const createVue = (store: StoreInstance, router: RouterInstance): Vue => {
     ViewportControl.disableSelection();
   }
   ViewportControl.init();
-
-  // Register the router hooks with their names
-  Component.registerHooks([
-    'asyncData',
-    'onWechatReady',
-    'beforeRouteEnter',
-    'beforeRouteLeave',
-    'beforeRouteUpdate', // for vue-router 2.2+
-  ]);
-
-  // Init Vue
-  Vue.config.productionTip = false;
-  Vue.use(StoreUtils);
-  Vue.use(PortalVue);
-  Vue.use(PreventOverscroll);
-  Vue.use(Wechat);
-  Vue.use(PhotoSwipe, {
-    pswpOptions: {
-      showShare: false,
-      showZoom: false,
-      showArrow: false,
-      showFullscreen: false,
-    },
-    wechat,
-  });
-  Vue.use(VueLazyload);
-
-  Object.defineProperty(Vue.prototype, '$routeInfo', { get() { return routeClone(this.$route); } });
-
-  Vue.mixin(Vue.extend({
-    beforeMount() {
-      // Deal with Vue.use() of current component.
-      const uses = this.$options.uses;
-      if (uses) {
-        uses.forEach(entity => Vue.use(entity));
-      }
-    },
-    mounted() {
-      const proto = Object.getPrototypeOf(this);
-      const options = this.$options.options || get(proto, 'constructor.options.options');
-      if (options) {
-        if (options.hideTabbar !== void 0) {
-          store.commit(`common/bus/${COMMON.SET_TABBAR_VISIBLE}`, { id: this._uid, value: !options.hideTabbar });
-        }
-        if (options.hideNavbar !== void 0) {
-          store.commit(`common/bus/${COMMON.SET_NAVBAR_VISIBLE}`, { id: this._uid, value: !options.hideNavbar });
-        }
-        if (options.bodyAutoHeight !== void 0) {
-          store.commit(`common/bus/${COMMON.SET_BODY_AUTO_HEIGHT}`, { id: this._uid, value: options.bodyAutoHeight });
-        }
-        if (options.bodyBackground !== void 0) {
-          store.commit(`common/bus/${COMMON.SET_BODY_BACKGROUND}`, { id: this._uid, value: options.bodyBackground });
-        }
-      }
-      const onWechatReady = this.$options.onWechatReady;
-      if (onWechatReady) {
-        configWechatSDK(store).then(() => {
-          this.$wechat.ready(() => {
-            onWechatReady.call(this);
-          });
-        });
-      }
-    },
-    destroyed() {
-      const proto = Object.getPrototypeOf(this);
-      const options = this.$options.options || get(proto, 'constructor.options.options');
-      if (options) {
-        if (options.hideTabbar !== void 0) {
-          store.commit(`common/bus/${COMMON.REMOVE_TABBAR_VISIBLE}`, { id: this._uid });
-        }
-        if (options.hideNavbar !== void 0) {
-          store.commit(`common/bus/${COMMON.REMOVE_NAVBAR_VISIBLE}`, { id: this._uid });
-        }
-        if (options.bodyAutoHeight !== void 0) {
-          store.commit(`common/bus/${COMMON.REMOVE_BODY_AUTO_HEIGHT}`, { id: this._uid });
-        }
-        if (options.bodyBackground !== void 0) {
-          store.commit(`common/bus/${COMMON.REMOVE_BODY_BACKGROUND}`, { id: this._uid });
-        }
-      }
-    },
-  }));
 
   sync(store, router);
   return new Vue({
