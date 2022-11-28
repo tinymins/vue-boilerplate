6fb78835bdd711f7ecfb19cd40ecf11b78e9ce6f
Emil Zhai <root@derzh.com>
2022-11-28 15:28:54 +0800

    fix: lint errors after bump lint package

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,6 +6,16 @@
  * @copyright: Copyright (c) 2018 TINYMINS.
  */
 
+const selectorClassPattern = [
+  // Matches class name likes this: block__elem--mod or block1__elem1--mod1-block2__elem2--mod2-...
+  /^(?:(weui|ant|BraftEditor)-[a-zA-Z-_]+|(?:(?:(?:^|(?!^)-)[a-z]+\d*|-[a-z]*\d+)(?:__[a-z]+\d*|__[a-z]*\d+){0,1}(?:--[a-z]+\d*|--[a-z]*\d+){0,1})*)$/u,
+  {
+    severity: 'error',
+    resolveNestedSelectors: true,
+    message: 'Selector should be written in BEM style (selector-class-pattern)',
+  },
+];
+
 module.exports = {
   extends: 'stylelint-config-standard',
   plugins: [
@@ -40,15 +50,7 @@ module.exports = {
     'no-empty-source': null,
     'no-descending-specificity': null,
     'number-leading-zero': 'never',
-    'selector-class-pattern': [
-      // Matches class name likes this: block__elem--mod or block1__elem1--mod1-block2__elem2--mod2-...
-      /^(?:(weui|ant|BraftEditor)-[a-zA-Z-_]+|(?:(?:(?:^|(?!^)-)[a-z]+\d*|-[a-z]*\d+)(?:__[a-z]+\d*|__[a-z]*\d+){0,1}(?:--[a-z]+\d*|--[a-z]*\d+){0,1})*)$/u,
-      {
-        severity: 'error',
-        resolveNestedSelectors: true,
-        message: 'Selector should be written in BEM style (selector-class-pattern)',
-      },
-    ],
+    'selector-class-pattern': selectorClassPattern,
     'selector-id-pattern': /^\$?[a-z][a-z0-9]*(?:-[a-z0-9]+)*$/u,
     'selector-max-compound-selectors': null,
     'selector-no-qualifying-type': null,
@@ -67,13 +69,14 @@ module.exports = {
       files: ['*.less', '**/*.less'],
       customSyntax: 'postcss-less',
       rules: {
+        'import-notation': null,
         'selector-class-pattern': null,
       },
     },
     {
       files: ['src/styles/*.less', 'src/styles/**/*.less'],
       rules: {
-        'selector-class-pattern': null,
+        'selector-class-pattern': selectorClassPattern,
       },
     },
     {
@@ -81,26 +84,28 @@ module.exports = {
       customSyntax: 'postcss-scss',
       extends: 'stylelint-config-sass-guidelines',
       rules: {
+        'import-notation': null,
         'selector-class-pattern': null,
       },
     },
     {
       files: ['src/styles/*.sass', 'src/styles/**/*.sass'],
       rules: {
-        'selector-class-pattern': null,
+        'selector-class-pattern': selectorClassPattern,
       },
     },
     {
       files: ['*.scss', '**/*.scss'],
       customSyntax: 'postcss-scss',
       rules: {
+        'import-notation': null,
         'selector-class-pattern': null,
       },
     },
     {
       files: ['src/styles/*.scss', 'src/styles/**/*.scss'],
       rules: {
-        'selector-class-pattern': null,
+        'selector-class-pattern': selectorClassPattern,
       },
     },
   ],
